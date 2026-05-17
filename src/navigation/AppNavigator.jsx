00593fd82a99bf6b2124fccede272d61b55537f8
GunashekarRM1 <reddygunashekar.d@gmail.com>
2026-05-17 20:01:36 +0530

    services

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import BottomTabs from './BottomTabs';
 
+import HomeScreen from '../screens/HomeScreen';
 import GuidesScreen from '../screens/GuidesScreen';
 import RoomsScreen from '../screens/RoomsScreen';
 import VehiclesScreen from '../screens/VehiclesScreen';
@@ -13,12 +14,24 @@ import PlacesScreen from '../screens/PlacesScreen';
 import MapScreen from '../screens/MapScreen';
 import NewsScreen from '../screens/NewsScreen';
 
+import TemplesPage from '../screens/TemplesPage';
+import AartiSlotsPage from '../screens/AartiSlotsPage';
+import RoomsPage from '../screens/RoomsPage';
+import GuidesPage from '../screens/GuidesPage';
+
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="MainTabs" component={BottomTabs} />
+
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Temples" component={TemplesPage} />
+      <Stack.Screen name="AartiSlots" component={AartiSlotsPage} />
+      <Stack.Screen name="RoomDetails" component={RoomsPage} />
+      <Stack.Screen name="GuideDetails" component={GuidesPage} />
+
       <Stack.Screen name="Guides" component={GuidesScreen} />
       <Stack.Screen name="Rooms" component={RoomsScreen} />
       <Stack.Screen name="Vehicles" component={VehiclesScreen} />
